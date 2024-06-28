a92906a7a5c85337200b0debe6ccce789abe50d7
skapa <skapa711@gmail.com>
2024-06-28 17:20:58 -0400

    updated and refactored code

--- Code/Visualizer.java
+++ Code/Visualizer.java
@@ -23,29 +23,25 @@ public class Visualizer extends JPanel implements Runnable {
     private double graphHeight;
     private int scaling = 25;
     private int ovalSize = 6;
-    private List<Integer> tsp1;
-    private List<Integer> tsp2;
+    private List<Integer> algo;
     private List<Edge> yang;
-    private List<Integer> marl;
-    private List<Integer> csp1;
-    private List<Integer> csp2;
+    
 
-    public Visualizer(List<Node> nodes, double width, double height, List<Integer> tsp1, List<Integer> tsp2, List<Edge> yang, List<Integer> marl) {
+    public Visualizer(List<Node> nodes, double width, double height, List<Integer> algo) {
         this.nodes = nodes;
         this.graphWidth = width;
         this.graphHeight = height;
-        this.tsp1 = tsp1;
-        this.tsp2 = tsp2;
-        this.yang = yang;
-        this.marl = marl;
+        this.algo = algo;
         invalidate();
         repaint();
     }
-    public Visualizer(List<Node> nodes, double width, double height, List<Edge> yang) {
+    public Visualizer(List<Node> nodes,List<Edge> yang, double width, double height) {
         this.nodes = nodes;
         this.graphWidth = width;
         this.graphHeight = height;
         this.yang = yang;
+        invalidate();
+        repaint();
     }
 
     @Override
@@ -54,14 +50,8 @@ public class Visualizer extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        /* double xScale = ((getWidth() - 3 * scaling) / (graphWidth));
-        double yScale = ((getHeight() - 3 * scaling) / (graphHeight)); */
         double xScale = ((getWidth() - 3 * scaling) / (graphWidth));
         double yScale = ((getHeight() - 3 * scaling) / (graphHeight));
-
-        // Shift the origin point up by 50 pixels
-        /* double xOffset = 0;
-        double yOffset = -50; */
 
         List<Point> graphPoints = new ArrayList<Point>();
         for (Node node : nodes) {
@@ -71,15 +61,6 @@ public class Visualizer extends JPanel implements Runnable {
             point.setLocation(x1, y1);
             graphPoints.add(point);
         }
-
-        /* List<Point> graphPoints = new ArrayList<Point>();
-        for (Node node : nodes) {
-            double x1 = (node.getX() * (xScale) + (2 * scaling));
-            double y1 = ((graphHeight - node.getY()) * yScale + scaling);
-            Point point = new Point();
-            point.setLocation(x1, y1);
-            graphPoints.add(point);
-        } */
 
         g2.setColor(Color.white);
         g2.fillRect(0, 0, getWidth(), getHeight());
@@ -92,17 +73,14 @@ public class Visualizer extends JPanel implements Runnable {
             double ovalW = ovalSize*4;
             double ovalH = ovalSize*4;
             Ellipse2D.Double shape = new Ellipse2D.Double(x, y, ovalW, ovalH);
-            g2.fill(shape);
-            
+            g2.fill(shape);  
         }
 
         // Draw node IDs in red with increased spacing
         g2.setColor(Color.red);
         FontMetrics metrics = g2.getFontMetrics();
-        
         int textHeight = metrics.getHeight();
-        int textPadding = 30; // Adjust space between id and node
-        int idSpac = 10; 
+        int textPadding = 30; // Adjust space between id and node 
         Font originalFont = g2.getFont();
         Font largerFont = originalFont.deriveFont(originalFont.getSize() + 20f); // adjust for font size
         g2.setFont(largerFont);
@@ -121,105 +99,32 @@ public class Visualizer extends JPanel implements Runnable {
             g2.drawString(String.format("(%d)", nodes.get(i).getDataPackets()), (int) xData, (int) y);
         }
         
-      
-       
-
         Stroke stroke = new BasicStroke(2f);
         g2.setStroke(stroke);
 
-        // Draw route 1 (dark yellow)
-        /* g2.setColor(Color.green); // gTSP1
-            for (int j = 0; j < tsp1.size() - 1; j++) {
-            int nodeIndex1 = tsp1.get(j);
-            int nodeIndex2 = tsp1.get(j + 1);
+        // Use for TSP,CSP and MARL
+        g2.setColor(Color.green); 
+            for (int j = 0; j < algo.size() - 1; j++) {
+            int nodeIndex1 = algo.get(j);
+            int nodeIndex2 = algo.get(j + 1);
             Point point1 = graphPoints.get(nodeIndex1);
             Point point2 = graphPoints.get(nodeIndex2);
             g2.drawLine(point1.x, point1.y, point2.x, point2.y);
         }
- */
-        // Draw route 2 (dark green)
-       /*  g2.setColor(Color.orange); // gTSP2
-            for (int j = 0; j < tsp2.size() - 1; j++) {
-            int nodeIndex1 = tsp2.get(j);
-            int nodeIndex2 = tsp2.get(j + 1);
-            Point point1 = graphPoints.get(nodeIndex1);
-            Point point2 = graphPoints.get(nodeIndex2);
-            g2.drawLine(point1.x, point1.y, point2.x, point2.y);
-        } */
 
-        g2.setColor(Color.RED); // yangs
+        // Use for Yangs
+        g2.setColor(Color.RED);
             for (int j = 0; j < yang.size(); j++) {
             int nodeIndex1 = yang.get(j).start.getID();
             int nodeIndex2 = yang.get(j).end.getID();
             Point point1 = graphPoints.get(nodeIndex1);
             Point point2 = graphPoints.get(nodeIndex2);
             g2.drawLine(point1.x, point1.y, point2.x, point2.y);
-        }
-
-       /*  g2.setColor(Color.BLUE); // marl
-            for (int j = 0; j < marl.size() - 1; j++) {
-            int nodeIndex1 = marl.get(j);
-            int nodeIndex2 = marl.get(j + 1);
-            Point point1 = graphPoints.get(nodeIndex1);
-            Point point2 = graphPoints.get(nodeIndex2);
-            g2.drawLine(point1.x, point1.y, point2.x, point2.y);
-        } */
-
-        /* g2.setColor(Color.MAGENTA); // csp1
-            for (int j = 0; j < csp1.size() - 1; j++) 
-            {
-                int nodeIndex1 = csp1.get(j);
-                int nodeIndex2 = csp1.get(j + 1);
-                Point point1 = graphPoints.get(nodeIndex1);
-                Point point2 = graphPoints.get(nodeIndex2);
-                g2.drawLine(point1.x, point1.y, point2.x, point2.y);
-            }
-
-            // Draw circles around points in csp1 route
-            g2.setColor(Color.gray);
-            double averageScale = (xScale + yScale) / 2;
-            double circleRadius = 700 * averageScale;
-            
-            for (int j = 0; j < csp1.size(); j++) {
-                int nodeIndex = csp1.get(j);
-                Point point = graphPoints.get(nodeIndex);
-                double x = point.x - circleRadius / 2;
-                double y = point.y - circleRadius / 2;
-                double circleW = circleRadius;
-                double circleH = circleRadius;
-                Ellipse2D.Double shape = new Ellipse2D.Double(x, y, circleW, circleH);
-                g2.draw(shape);
-            } */
-
-       /*  g2.setColor(Color.CYAN); // csp2
-            for (int j = 0; j < csp2.size() - 1; j++) 
-            {
-                int nodeIndex1 = csp2.get(j);
-                int nodeIndex2 = csp2.get(j + 1);
-                Point point1 = graphPoints.get(nodeIndex1);
-                Point point2 = graphPoints.get(nodeIndex2);
-                g2.drawLine(point1.x, point1.y, point2.x, point2.y);
-            }
-        
-            g2.setColor(Color.gray);
-            for (int j = 0; j < csp2.size(); j++) {
-                int nodeIndex = csp2.get(j);
-                Point point = graphPoints.get(nodeIndex);
-                double x = point.x - circleRadius;
-                double y = point.y - circleRadius;
-                double circleW = circleRadius;
-                double circleH = circleRadius;
-                Ellipse2D.Double shape = new Ellipse2D.Double(x, y, circleW, circleH);
-                g2.draw(shape);
-            }
- */
-
-            // Helper method to calculate the radius in pixels based on the given radius in meters
-          
-        
+        }        
     }
 
-    public void run() {
+    public void run() 
+    {
         String graphName = "Sensor Network Graph";
         JFrame frame = new JFrame(graphName);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
